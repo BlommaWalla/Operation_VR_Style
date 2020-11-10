9330b6a10f550d549650de2affaf1570cf33735c
BlommaWalla <lutz.hahlbeck@gmail.com>
2020-11-10 14:57:48 +0100

    gun script gebinning

---
+++ Operation_VR_Style_test/Assets/Scripts/Blaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster : MonoBehaviour {
+
+    public GameObject m_ProjectilePrefab = null;
+
+    private ProjectilePool m_ProjectilePool = null;
+
+    private void Awake() {
+        m_ProjectilePool = new ProjectilePool(m_ProjectilePrefab, 10);
+    }
+}
