1bb1de6dd7f6cf49f6f5d143c8e80ee60885a6c2
BlommaWalla <lutz.hahlbeckmail.com>
2020-11-17 14:56:36 +0100

    fHjklkjkhjghfg

--- Operation_VR_Style_test/Assets/Shooting.cs
+++ Operation_VR_Style_test/Assets/Shooting.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 using VRTK;
 public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject bullet;
+
     void Start()
     {
         GetComponent<VRTK_InteractableObject>().InteractableObjectUsed += Shoot;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void Shoot(object sender, InteractableObjectEventArgs e) {
+        Debug.Log("pang");
+        Instantiate(bullet, new Vector3(0, 0, 0), Quaternion.identity);
+
 
     }
+
+
 }
