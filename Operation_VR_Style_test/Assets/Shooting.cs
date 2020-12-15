7e6a215ee535a49b9dc720db192235aa92295b95
BlommaWalla <lutz.hahlbeck@gmail.com>
2020-12-15 15:37:08 +0100

    kaabaa

--- Operation_VR_Style_test/Assets/Shooting.cs
+++ Operation_VR_Style_test/Assets/Shooting.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
-public class Shooting : MonoBehaviour
-{
+public class Shooting : MonoBehaviour {
     public GameObject bullet;
     public Transform barrel;
 
-    void Start()
-    {
+    private GameObject m_projectile;
+    void Start() {
         GetComponent<VRTK_InteractableObject>().InteractableObjectUsed += Shoot;
     }
 
     void Shoot(object sender, InteractableObjectEventArgs e) {
         Debug.Log("pang");
-        Instantiate(bullet, barrel.position, barrel.rotation);
+        m_projectile = Instantiate(bullet, barrel.position, barrel.rotation);
 
 
     }
-
-
 }
